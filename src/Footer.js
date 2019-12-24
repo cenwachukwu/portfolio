5b83085c651f04c9933698b56c26c55a28cf59e0
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-12-24 01:00:46 -0500

    updated footer with codepen

--- src/Footer.js
+++ src/Footer.js
@@ -7,6 +7,7 @@ function Footer() {
             <div className="footerDiv">
                 <a href="https://github.com/cenwachukwu" target="_blank"><i class="fab fa-github"></i></a>
                 <a href="https://www.linkedin.com/in/cenwachukwu" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                <a href="#" target="_blank"><i class="fab fa-codepen"></i></a>
             </div>
         </div>
     );
