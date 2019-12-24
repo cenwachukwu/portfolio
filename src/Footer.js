49a50beb358212b1bb0620a5c99e80a585795aed
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-12-24 00:58:14 -0500

    created footer with links

--- src/Footer.js
+++ src/Footer.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 
 function Footer() {
-  return (
-    <div className="App">
-      <h2>Footer</h2>
-    </div>
-  );
+    return (
+        <div className="footerDivContainer">
+            <div className="footerDiv">
+                <a href="https://github.com/cenwachukwu" target="_blank"><i class="fab fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/cenwachukwu" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+            </div>
+        </div>
+    );
 }
 
 export default Footer;
