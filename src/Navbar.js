86a323a068242689f9ebcb8a224120a4722e81dc
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-12-24 00:30:49 -0500

    created fixed navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,27 +4,29 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
-        <div className="Navbar">
-            <div>
-                <Link to="/">
-                    <h2>CEN</h2>
-                </Link>
-            </div>
-            <div className="NavLinks">
-                <ul className="NavBar">
-                    <li>
-                        <Link to="/#About">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/#Portfolio">Portfolio</Link>
-                    </li>
-                    <li>
-                        <Link to="/Resume">Resume</Link>
-                    </li>
-                    <li>
-                        <Link to="/Contact">Contact</Link>
-                    </li>
-                </ul>
+        <div className="navContainer">
+            <div className="navBarContainer">
+                <div>
+                    <Link to="/">
+                        <h2 className="homeLogo">cenwachukwu</h2>
+                    </Link>
+                </div>
+                <div className="NavLinks">
+                    <ul className="NavBarLinks">
+                        <li>
+                            <Link to="/#About">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/#Portfolio">Portfolio</Link>
+                        </li>
+                        <li>
+                            <Link to="/Resume">Resume</Link>
+                        </li>
+                        <li>
+                            <Link to="/Contact">Contact</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
 
