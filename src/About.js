5105abe7d0d6ff4d619a3d0e00b74913a59e52a2
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-12-24 21:03:33 -0500

    added the about page

--- src/About.js
+++ src/About.js
@@ -23,7 +23,92 @@ function About() {
                 <div className="aboutCatchPhrase2">
                     <h3>My Story</h3>
                 </div>
+                <p>Since my background is a little unusual, I’m usually asked how I got involved with programming. Here’s how:</p>
+                <div className="StoryDivContainerParent">
+                    <div className="StoryDivContainer">
+                        <div className="StoryBox">
+                            <div className="storyBoxNumber">
+                                <p>1</p>
+                            </div>
+                            <div className="storyBoxIcon">
+                                <i class="fas fa-plane-departure storyIcon"></i>
+                            </div>
+                            <div className="storyBoxTextBox">
+                                <p className="storyBoxText">
+                                    In 2013, I moved from Lagos, Nigeria to Washington, DC to pursue a bachelors degree in Biology.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="StoryBox">
+                            <div className="storyBoxNumber">
+                                <p>2</p>
+                            </div>
+                            <div className="storyBoxIcon">
+                                <i class="fas fa-user-graduate storyIcon"></i>
+                            </div>
+                            <div className="storyBoxTextBox">
+                                <p className="storyBoxText">
+                                    Then i graduated and realized... now what?
+                                </p>
+                            </div>
 
+                        </div>
+                        <div className="StoryBox">
+                            <div className="storyBoxNumber">
+                                <p>3</p>
+                            </div>
+                            <div className="storyBoxIcon">
+                                <i class="fab fa-python storyIcon"></i>
+                            </div>
+                            <div className="storyBoxTextBox">
+                                <p className="storyBoxText">
+                                    Fastforward to 2018, I started working with a tech start-up and out of curiosity, I started teching myself python.
+                                </p>
+                            </div>
+
+                        </div>
+                        <div className="StoryBox">
+                            <div className="storyBoxNumber">
+                                <p>4</p>
+                            </div>
+                            <div className="storyBoxIcon">
+                                <i class="far fa-smile-beam storyIcon"></i>
+                            </div>
+                            <div className="storyBoxTextBox">
+                                <p className="storyBoxText four">
+                                My first python project – learning new information, the struggle of getting things to work, and how proud I was when it all came together - was all the push i needed to enroll in the Software Immersive Program at General Assembly DC.
+                                </p>
+                            </div>
+
+                        </div>
+                        <div className="StoryBox">
+                            <div className="storyBoxNumber">
+                                <p>5</p>
+                            </div>
+                            <div className="storyBoxIcon">
+                            <i class="fas fa-server storyIcon"></i>
+                            </div>
+                            <div className="storyBoxTextBox">
+                                <p className="storyBoxText">
+                                    Armed with skills in front- and back-end technologies, frameworks, tools, and methods, I'm ready to takeover the world.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="StoryBox">
+                            <div className="storyBoxNumber">
+                                <p>6</p>
+                            </div>
+                            <div className="storyBoxIcon">
+                            <i class="far fa-user storyIcon"></i>
+                            </div>
+                            <div className="storyBoxTextBox">
+                                <p className="storyBoxText">
+                                What's next? I want to keep growing my programming skills while fulfiling my passion of creating user-friendly and aesthetically pleasing applications. I also want to visit Croatia!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
