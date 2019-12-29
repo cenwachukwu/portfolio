69c77edc130b346026f1156a04fe63c68e1e913a
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2019-12-28 21:22:44 -0500

    finally made the modal

--- src/Applications.js
+++ src/Applications.js
@@ -2,94 +2,114 @@ import React, { useState } from "react";
 import './App.css';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import { Button, } from "@material-ui/core";
+import ProjectData from "./ProjectData"
 
 function Modal(props) {
-  console.log(props)
   return (
-    <div>
+    <div className="modal-wrapper" style={{
+      transform: props.openModal ? 'translateY(0vh)' : 'translateY(-100vh)',
+      opacity: props.openModal ? '1' : '0'
+    }}>
+      <div className="modal-header">
+        <span className="close-modal-btn" onClick={props.handleCloseModal}>×</span>
+      </div>
       {props.children}
     </div>
   )
 }
 
-function Project({ projects, openModal }) {
-  console.log(openModal)
-  const imageUrl = projects.backgroundImage;
+function Project({ project, handleOpenModal, }) {
+  const imageUrl = project.backgroundImage;
   return (
-    <Modal>
+    <div>
+      <Button onClick={() => handleOpenModal(project)}>I am a modal</Button>
       <div className="projectsBox">
         <div className="projectName">
           <div style={{ backgroundImage: `url(${imageUrl})` }} className="projectImage"></div>
-          <p>{projects.name}</p>
-        </div>
-        <div className="projectDescriptionDiv">
-          <div className="projectDescription">
-            <span className="descriptionSpan">Technologies:    </span>
-            <p>{projects.technologies}</p>
-          </div>
-          <div className="projectDescription">
-            <span className="descriptionSpan">Description:</span>
-            <p>{projects.description}</p>
-          </div>
-          <div className="projectDescription" id="stack">
-            <span className="descriptionSpan">GitHub:    </span>
-            <a href={projects.github} target="_blank" rel="noopener noreferrer" id="blacklinktag">{projects.github}</a>
-          </div>
+          <p onClick={() => handleOpenModal(project)}>{project.name}</p>
         </div>
       </div>
-    </Modal>
+    </div>
   )
 }
 
 function Applications() {
-  const [projects, setProjects] = useState([{
-    openModal: false,
-    projects: [
-      {
-        name: "Cotripper",
-        description: "Built an open-source full-stack application to facilitate an organized group travel experience for single mothers. My contributions included developing Banner and Modal component libraries and collaborating with a team of five engineers to build out the 'Book a trip' page.  Over 25 engineers collaborated on this project using agile (kanban) methodologies, morning and afternoon standing-ups, and GitHub workflow.",
-        technologies: "CSS, Django, Enzyme, Jest, Node.JS,  PostgreSQL, Python, React.JS, Storybook, SQL",
-        github: "https://github.com/CotripperPlatform/CoTrip",
-        Stack: "Django (Back-end), Reacto (Front-end), SQL (Storage)",
-        backgroundImage: require("./images/cotippeer.png"),
-        openModal: false
-      },
-      {
-        name: "theNotesCli",
-        description: "Developed a command-line application that has the ability to create, update and delete notes based on the user's request.",
-        technologies: "Peewee ORM, PostgreSQL, Psycopg2, Python",
-        github: "https://github.com/cenwachukwu/pythonCLIProject",
-        Stack: "Python",
-        backgroundImage: require("./images/meddirectoryapi.gif"),
-        openModal: false
-      },
-      {
-        name: "A Component Library ",
-        description: "Developed a library of different styles of buttons, forms, inputs, and image cards which saves users the time of building basic components from scratch.",
-        technologies: "Adobe XD, CSS,  NPM, React, Storybook",
-        github: "https://github.com/cenwachukwu/A-Component-Library",
-        Stack: "React",
-        backgroundImage: require("./images/storybook.png"),
-        openModal: false
-      },
-    ]
-  }]);
+  const [projects, setProjects] = useState([{ openModal: false, }]);
+
+  function handleOpenModalClick(project) {
+    const newProject = [...projects, { project }];
+    newProject[0].openModal = true;
+    setProjects(newProject)
+  }
+
+  function handleCloseModal() {
+    // console.log(projects)
+    alert('The modal was closed.');
+    let newProject = [...projects];
+    newProject[0].openModal = false;
+    newProject.splice(1, 1);
+    // console.log(newProject)
+    setProjects(newProject)
+  }
+
+  function renderProjects() {
+    return ProjectData.map((project, index) => {
+      return <Project
+        key={index}
+        index={index}
+        project={project}
+        openModal={projects[0].openModal}
+        handleOpenModal={handleOpenModalClick}
+        handleCloseModal={handleCloseModal} />
+    });
+  }
+
+  function renderModal() {
+    console.log(projects)
+    const projectModal = projects[0]
+    if (!projectModal.openModal) {
+      return null;
+    }
+    const imageUrl = projects[1].project.backgroundImage;
+    return (
+      <div>
+        <div className="modalBox">
+          <div className="projectName">
+            <div style={{ backgroundImage: `url(${imageUrl})` }} className="projectImage"></div>
+            <p className="projectnamewhite">{projects[1].project.name}</p>
+          </div>
+          <div className="projectDescriptionDiv">
+            <div className="projectDescription">
+              <span className="descriptionSpan">Technologies:    </span>
+              <p className="projectDescriptionP">{projects[1].project.technologies}</p>
+            </div>
+            <div className="projectDescription">
+              <span className="descriptionSpan">Description:</span>
+              <p className="projectDescriptionP">{projects[1].project.description}</p>
+            </div>
+            <div className="projectDescription" id="stack">
+              <a href={projects[1].project.github} target="_blank" rel="noopener noreferrer" id="blacklinktag"><i className="fab fa-github"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <div>
       <Navbar />
+      {projects[0].openModal ? <div onClick={handleCloseModal} className="back-drop-close-modal"></div> : null}
       <div className="projectsParentsContainer">
-        {projects[0].showModal ? <div className="back-drop-close-modal"></div> : null}
+      {projects[0].openModal ? null : 
         <div className="projectsParents">
-          {projects[0].projects.map((project, index) => (
-            <Project
-              key={index}
-              index={index}
-              projects={project}
-              openModal={projects[0].openModal}
-            />
-          ))}
-        </div>
+          {renderProjects()}
+        </div>}
+        {projects[0].openModal ? 
+        <Modal openModal={projects[0].openModal} handleCloseModal={handleCloseModal}>
+          {renderModal()}
+        </Modal> : null}
       </div>
       <Footer />
     </div>
---
+++ src/ProjectData.js
@@ -0,0 +1,28 @@
+export default [
+
+  {
+    name: "Cotripper",
+    description: "Built an open-source full-stack application to facilitate an organized group travel experience for single mothers. My contributions included developing Banner and Modal component libraries and collaborating with a team of five engineers to build out the 'Book a trip' page.  Over 25 engineers collaborated on this project using agile (kanban) methodologies, morning and afternoon standing-ups, and GitHub workflow.",
+    technologies: "CSS, Django, Enzyme, Jest, Node.JS,  PostgreSQL, Python, React.JS, Storybook, SQL",
+    github: "https://github.com/CotripperPlatform/CoTrip",
+    Stack: "Django (Back-end), Reacto (Front-end), SQL (Storage)",
+    backgroundImage: require("./images/cotippeer.png"),
+  },
+  {
+    name: "theNotesCli",
+    description: "Developed a command-line application that has the ability to create, update and delete notes based on the user's request.",
+    technologies: "Peewee ORM, PostgreSQL, Psycopg2, Python",
+    github: "https://github.com/cenwachukwu/pythonCLIProject",
+    Stack: "Python",
+    backgroundImage: require("./images/meddirectoryapi.gif"),
+  },
+  {
+    name: "A Component Library ",
+    description: "Developed a library of different styles of buttons, forms, inputs, and image cards which saves users the time of building basic components from scratch.",
+    technologies: "Adobe XD, CSS,  NPM, React, Storybook",
+    github: "https://github.com/cenwachukwu/A-Component-Library",
+    Stack: "React",
+    backgroundImage: require("./images/storybook.png"),
+  },
+
+]
